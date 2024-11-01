4b13461252e4032b5096f529e6cbe5ac47dfb2c5
drag0y <adragoy88@gmail.com>
2024-11-01 14:42:33 +0300

    Fix function get CATV

--- functions/gpon/get_catv_state_gpon.py
+++ functions/gpon/get_catv_state_gpon.py
@@ -4,7 +4,7 @@ def get_catvstate_gpon(olt_ip, portid, onuid, snmp_com):
 # --- Функция для получения состояния CATV порта, включен или выключен в конфигурации
     try:
         catvstatusoid = "1.3.6.1.4.1.2011.6.128.1.1.2.63.1.2"
-        catvstatcmd = f"snmpwalk -c {snmp_com} -v2c {olt_ip} {catvstatusoid}.{portid}.{onuid}"
+        catvstatcmd = f"snmpwalk -c {snmp_com} -v2c {olt_ip} {catvstatusoid}.{portid}.{onuid}.1"
         catvstatuscmd = catvstatcmd.split()
 
         process = subprocess.Popen(catvstatuscmd, stdout=subprocess.PIPE)
